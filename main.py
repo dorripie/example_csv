ec1354c44ebc7446606207fc0f85f482f154dbc2
dorripie <bondarenkoda@spc063.ru>
2022-11-29 18:55:17 +0300

    Написание функции save

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stud_csv import file_open
+from stud_csv import file_open, insert, show_rows, save
 
 FILENAME = "data (1).csv"
 
@@ -8,8 +8,9 @@ MENU = {
     '3': 'Удалить',
     '4': 'Поиск по ФИО',
     '5': 'Вывести из группы',
-    '6': 'Перевод на ку',
+    '6': 'Сохранить в файл',
     '7': 'Вывести данные',
+    '8': 'Перевести на курс',
     '0': 'Меню',
     'exit': 'Выход'
 }
@@ -21,5 +22,12 @@ while True:
     action = input('>_')
     if action == '1':
         file_open(FILENAME)
+    elif action == '2':
+        insert(input('ФИО: '), input('пол: '), input('возраст: '), input('телефон: '), input('почта: '), input('группа: '), input('курс: '))
+    elif action == '6':
+        save(FILENAME)
+    elif action == '7':
+        show_rows()
+
     elif action == 'exit':
         break
--- stud_csv.py
+++ stud_csv.py
@@ -9,3 +9,36 @@ def file_open(file_name):
         for row in reader:
             csv_file.append(row)
         print("Файл открыт. Записей: ", len(csv_file))
+
+# Добавление
+def insert(fio, pol, age, tel, mail, group, well):
+    try:
+        mx = max(csv_file, key=lambda x: int(x['номер']))
+        csv_file.append({'номер': int(mx['номер'])+1,
+                         'ФИО': fio,
+                         'пол': pol,
+                         'возраст': age,
+                         'телефон': tel,
+                         'почта': mail,
+                         'группа': group,
+                         'курс': well})
+    except Exception as e:
+        return f"Ошибка при добавлении нового студента {e}"
+    print("Данные добавлены")
+
+# Сохранить в файл
+def save(fine_name):
+    with open(fine_name, 'w', encoding='utf-8', newline='') as file:
+        columns = ['номер','ФИО','пол','возраст','телефон','почта','группа','курс']
+        writer = csv.DictWriter (file, delimiter=";", fieldnames=columns)
+        writer.writeheader()
+        writer.writerows(csv_file)
+        print('Данные сохранены')
+
+# Вывод данных
+def show_rows():
+    if len(csv_file) > 0:
+        print('{:<5}{:<25}{:<3}{:<3}{:<11}{:<20}{:<5}{:<1}'.format('номер','ФИО','пол','возраст','телефон','почта','группа','курс'))
+        for el in csv_file:
+            print('{:<5}{:<25}{:<3}{:<3}{:<11}{:<20}{:<5}{:<1}'.format(el['номер'], el['ФИО'], el['пол'], el['возраст'], el['телефон'],
+                                                        el['почта'], el['группа'], el['курс']))
